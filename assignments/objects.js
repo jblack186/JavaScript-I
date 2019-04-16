ab8de0c5aa45838f757f54b4c28188dcd55db0ea
Jamison Blackwell <blackwellj1040@gmail.com>
2019-04-16 07:40:19 -0400

    added intern objects

--- assignments/objects.js
+++ assignments/objects.js
@@ -11,13 +11,49 @@
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
 const example = {
-  "id": 0,
+  "id": 1,
   "name": "Example",
   "email": "examples@you.edu",
   "gender": "F"
 }
 
 // Write your intern objects here:
+const intern1 = {
+  "id": 0,
+  "name": "Mitzi",
+  "email": "mmelloy0@psu.edu",
+  "gender": "F"
+}
+  
+const intern2 = {
+  "id": 2,
+  "name": "Kennan",
+  "email": "kdiben1@tinypic.com",
+  "gender": "M"
+}
+
+const intern3 = {
+  "id": 3,
+  "name": "Keven",
+  "email": "kmummery2@wikimedia.org",
+  "gender": "M"
+}
+
+const intern4 = {
+  "id": 4,
+  "name": "Gannie",
+  "email": "gmartinson3@illinois.edu",
+  "gender": "M"
+}
+
+const intern5 = {
+  "id": 5,
+  "name": "Antonietta",
+  "email": "adaine5@samsung.com",
+  "gender": "F"
+
+  
+}
 
 
 // ==== Challenge 2: Reading Object Data ==== 
